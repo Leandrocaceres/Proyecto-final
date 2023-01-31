356dc0bc6bfb6145ff0224b921a8b56fcfc8fc25
Leandro Caceres <ozakitshio@gmail.com>
2023-01-31 19:48:25 -0300

    se agrega el proyecto

---
+++ Java/java.js
@@ -0,0 +1,11 @@
+
+
+let pepe12 = document.getElementById('pepe12');
+
+let Experiencia_de_trabajo = document.getElementById('Experiencia_de_trabajo');
+
+pepe12.addEventListener('click', apretar);
+
+function apretar() {
+Experiencia_de_trabajo.classList.toggle('show');
+}
